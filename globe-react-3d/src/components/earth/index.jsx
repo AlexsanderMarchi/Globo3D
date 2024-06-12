eb8cb43357358f988da160f6980b4d649bfb2ddb
Alexsander Marchi Zunino <alexsandermarchi@gmail.com>
2024-06-11 22:49:57 -0300

    added latitude and longitude coordinates when clicked

--- globe-react-3d/src/components/earth/index.jsx
+++ globe-react-3d/src/components/earth/index.jsx
@@ -1,5 +1,6 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { useFrame, useLoader } from "@react-three/fiber";
+import { useThree, extend } from "react-three-fiber";
 import { TextureLoader } from "three";
 import { OrbitControls, Stars } from "@react-three/drei";
 import * as THREE from "three";
@@ -18,15 +19,60 @@ export function Earth(props) {
   const earthRef = useRef();
   const cloudsRef = useRef();
 
-  useFrame(({ clock }) => {
-    const elapsedTime = clock.getElapsedTime();
-    earthRef.current.rotation.y = elapsedTime / 6;
-    cloudsRef.current.rotation.y = elapsedTime / 6;
-  });
+  //   useFrame(({ clock }) => {
+  //     const elapsedTime = clock.getElapsedTime();
+  //     earthRef.current.rotation.y = elapsedTime / 6;
+  //     cloudsRef.current.rotation.y = elapsedTime / 6;
+  //   });
+
+  //CLICK COORDINATES
+
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const { camera, gl } = useThree();
+
+  const handleClick = (event) => {
+    const raycaster = new THREE.Raycaster();
+    const mouseVector = new THREE.Vector2();
+
+    // Set mouse coordinates
+    // mouseVector.x = mouse.x * 2 - 1;
+    // mouseVector.y = -(mouse.y * 2) + 1;
+    mouseVector.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouseVector.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+    // Update the picking ray with the camera and mouse position
+    raycaster.setFromCamera(mouseVector, camera);
+
+    // Calculate objects intersecting the picking ray
+    const intersects = raycaster.intersectObject(earthRef.current);
+
+    if (intersects.length > 0) {
+      // Get the intersection point
+      const intersectionPoint = intersects[0].point;
+
+      // Convert the intersection point to latitude and longitude
+      const latLong = pointToLatLong(intersectionPoint);
+      setSelectedCountry(latLong);
+      console.log("Latitude and Longitude: ", latLong);
+    }
+  };
+
+  const pointToLatLong = (point) => {
+    const radius = 1; // Radius of the Earth sphere
+    const phi = Math.acos(point.y / radius); // phi (latitude)
+    const theta = Math.atan2(point.z, point.x); // theta (longitude)
+
+    // Convert phi and theta to degrees
+    const lat = 90 - (phi * 180) / Math.PI;
+    const lon = (((theta * 180) / Math.PI + 180) % 360) - 180;
+
+    return { lat, lon };
+  };
+
   return (
     <>
-      {/* <ambientLight intensity={2} /> */}
-      <pointLight color="#f6f3ea" position={[2, 0, 2]} intensity={14.2} />
+      <ambientLight intensity={2} />
+      {/* <pointLight color="#f6f3ea" position={[2, 0, 2]} intensity={14.2} /> */}
       <Stars
         radius={300}
         depth={60}
@@ -35,7 +81,7 @@ export function Earth(props) {
         saturation={0}
         fade={true}
       />
-      <mesh ref={cloudsRef}>
+      <mesh ref={cloudsRef} onClick={handleClick}>
         <sphereGeometry args={[1.005, 32, 32]} />
         <meshPhongMaterial
           map={cloudsMap}
@@ -45,7 +91,7 @@ export function Earth(props) {
           side={THREE.DoubleSide}
         />
       </mesh>
-      <mesh ref={earthRef}>
+      <mesh ref={earthRef} onClick={handleClick}>
         <sphereGeometry args={[1, 32, 32]} />
         <meshPhongMaterial specularMap={specularMap} />
         <meshStandardMaterial
